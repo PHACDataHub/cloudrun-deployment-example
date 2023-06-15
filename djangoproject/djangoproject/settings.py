21487b5d36ac1cb7fa82232ab800b49a4f1bb6e6
Lila Kelland <lila.kelland@gmail.com>
2023-06-15 09:06:11 -0400

    followed Dan's instructions to connect cloud run to cloud SQL

--- djangoproject/djangoproject/settings.py
+++ djangoproject/djangoproject/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 import environ
+import psycopg2
+from urllib.parse import urlparse
 
 env = environ.Env()
 environ.Env.read_env()
@@ -91,11 +93,25 @@ WSGI_APPLICATION = 'djangoproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         # 'NAME': BASE_DIR / 'db.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+db_url = os.getenv('hello-world-env-secret-DATABASE_URL')
+
+url = urlparse(db_url)
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': url.path[1:],
+        'USER': url.username,
+        'PASSWORD': url.password,
+        'HOST': url.hostname,
+        'PORT': url.port,
     }
 }
 
@@ -109,6 +125,7 @@ DATABASES = {
 #         "PORT": os.environ.get("SQL_PORT", "5432"),
 #     }
 # }
+
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql_psycopg2',
