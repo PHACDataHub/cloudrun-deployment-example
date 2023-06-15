a62b408d74f8c230a988fc6e345be24e1d93a13e
Lila Kelland <lila.kelland@gmail.com>
2023-06-15 09:10:56 -0400

    added secret accessor role to cloud run to be able to access DB_URL in GCP secrets

--- djangoproject/deploy/initial-gcp-sa-api-setup.sh
+++ djangoproject/deploy/initial-gcp-sa-api-setup.sh
@@ -15,8 +15,9 @@ GITHUB_REPO_OWNER="PHACDataHub"
 CLOUD_BUILD_TRIGGER_NAME="hello-world-deploy-trigger"
 CLOUD_BUILD_CONFIG_PATH="djangoproject/deploy/cloudbuild.yaml"
 
-SQL_INSTANCE="hello-world-sql-instance"
+SQL_INSTANCE="hello-world-instance"
 DB_NAME="hello-world-db"
+DB_SECRET="hello-world-db-password"
 
 # Set google cloud storage bucket for static files storage
 GCS_BUCKET_NAME="hello_world_bucket"
@@ -75,27 +76,47 @@ gcloud projects add-iam-policy-binding $PROJECT_ID \
     --role="roles/cloudbuild.serviceAgent" \
     --role=roles/run.admin \
     --role=roles/artifactregistry.writer \
-    --role=roles/iam.serviceAccountUser
+    --role=roles/iam.serviceAccountUser \
+    --role=roles/secretmanager.secretAccessor
 
 
 # ---- CLOUD SQL (postgres) -----
+# Create database password:
+export DB_PASSWORD=$(openssl rand -base64 16 | tr -dc A-Za-z0-9 | head -c16 ; echo '')
+echo -n "${DB_PASSWORD}" | gcloud secrets create hello-world-db-password --replication-policy="user-managed" --locations="${REGION}" --data-file=-
+
 # Create SQL instance
 gcloud sql instances create $SQL_INSTANCE \
     --project $PROJECT_ID \
-    --database-version POSTGRES_13 \
-    --tier db-f1-micro \
-    --region $REGION
+    --database-version=POSTGRES_14 \
+    --tier=db-g1-small \
+    --region $REGION \
+    --root-password="${DB_PASSWORD}
 
 # Create Database
 gcloud sql databases create $DB_NAME \
     --instance $SQL_INSTANCE
 
-# Set DB username and password **MOVE TO SECRETS!
-gcloud sql users create postgres_user \
+# Set DB username and password
+gcloud sql users create hello-world-user \
     --instance $SQL_INSTANCE \
-    --password postgres_password
+    --password="${DB_PASSWORD}"
 
-# ----- CLOUD STORAGE BUCKET -----
+# Create instance connection 
+INSTANCE_CONNECTION_NAME=$(gcloud sql instances describe $SQL_INSTANCE --format='value(connectionName)')
+echo -n "postgresql://hello-world-user:${DB_PASSWORD}@localhost/${DB_NAME}?host=/cloudsql/${INSTANCE_CONNECTION_NAME}" | gcloud secrets create hello-world-db-connection-string --replication-policy="user-managed" --locations="${REGION}" --data-file=-
+
+# Don't forget to update (potentially)
+# 1- file:.env.prod-XXX
+# 2- secret: hello-world-env-secret-DATABASE_URL
+# create secret, if not exists:
+gcloud secrets create "hello-world-env-secret-DATABASE_URL" --replication-policy="user-managed" --locations="${REGION}"
+# Then update the secret with:
+echo -n "postgresql://hello-world-user:${DB_PASSWORD}@localhost/{DB_NAME}?host=/cloudsql/${INSTANCE_CONNECTION_NAME}" | gcloud secrets versions add "hello-world-env-secret-DATABASE_URL" --data-file=-
+# Or copy it from hello-world-db-connection-string
+gcloud secrets versions access latest --secret=hello-world-db-connection-string | gcloud secrets versions add "hello-world-env-secret-DATABASE_URL" --data-file=-
+
+# ----- CLOUD STORAGE BUCKET (for static files note - John found this for Django)-----
 # Enable API
 gcloud services enable \
     storage-component.googleapis.com \
@@ -103,5 +124,3 @@ gcloud services enable \
 
 # * Make bucket (needed to store .env?)
 gsutil mb -p $PROJECT_ID -l $REGION -b on gs://$GCS_BUCKET_NAME
-
-# Bind permissions for cloud run to talk to bucket 
